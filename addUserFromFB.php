9bb424bae894eb434f9018ecec7999d73ba78d89
shani <shaniabraham123@gmai.com>
2018-05-22 18:42:38 +0300

    fb

--- addUserFromFB.php
+++ addUserFromFB.php
@@ -10,7 +10,7 @@ mysql_select_db("GameOn",$con);
 
 $bool=mysql_query("INSERT INTO `User` (`user_id`,`first_name`, `last_name`, `password`, `date_of_birth`, `email`, `city`, `profile_picture`, `foot`, `speed`, `shot`, `dribble`, `fairness`, `header`, `goals`, `assits`, `team`)
    VALUES
-('$_POST[fbid]','$_POST[firstName]','$_POST[lastName]','$_POST[firstName]','$_POST[birthday]','$_POST[email]','$_POST[location]','$_POST[fbpic]' ,'R','0','0','0','0','0','0','0',NULL)");
+('$_POST[fbid]','$_POST[firstName]','$_POST[lastName]','$_POST[fbid]','$_POST[birthday]','$_POST[email]','$_POST[location]','$_POST[fbpic]' ,'R','0','0','0','0','0','0','0',NULL)");
 
 echo ($bool);
 mysql_close($con);
