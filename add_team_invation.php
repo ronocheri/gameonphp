b73418163287eda78b44c6e67be342cb0cf18c13
shani <shaniabraham123@gmai.com>
2018-07-07 22:24:30 +0300

    team

---
+++ add_team_invation.php
@@ -0,0 +1,17 @@
+
+<?php
+
+header('Content-Type: application/json');
+$con = mysql_connect( "173.194.106.92","gameon","shani123@");
+if(!$con){
+die("could not connect".mysql_error());
+}
+mysql_select_db("GameOn",$con);
+//mysql_select_db("GameOn",$con);
+$bool=mysql_query("INSERT INTO `Team_invitation` (`team_name`,`user_id`,`approve`)
+   VALUES
+('$_POST[t_id]','$_POST[picked_Users]','Null')");
+echo json_encode($bool);
+mysql_close($con);
+
+?>
