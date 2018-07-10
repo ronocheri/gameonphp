3d9d5e02cd44781df0b7ebd76e469b8998bbba00
shani <shaniabraham123@gmai.com>
2018-07-10 14:31:38 +0300

    team 10-7

--- answer_team_invitation.php
+++ answer_team_invitation.php
@@ -1,5 +1,8 @@
 <?php
 
+$team_name=$_POST["team_name"];
+$user_id=$_POST["user_id"];
+$user_choice=$_POST["user_choice"];
 
 header('Content-Type: application/json');
 $con = mysql_connect( "173.194.106.92","gameon","shani123@");
@@ -7,8 +10,9 @@ if(!$con){
 die("could not connect".mysql_error());
 }
 mysql_select_db("GameOn",$con);
-$result = mysql_query("SELECT * FROM Game_invitation
- WHERE game_id='$_POST[game_id]' and user_id='$_POST[user_id]'");
+$result = mysql_query("UPDATE `Team_invitation` SET `approve`='".$user_choice."'
+ WHERE team_name='".$team_name."' and user_id='".$user_id."'");
+
 $arr=array();
 while($row = mysql_fetch_assoc($result)){
 $arr[]=$row;
